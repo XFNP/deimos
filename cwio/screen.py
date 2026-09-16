e0b43abb26b2a3f36bf9de4f417d8d23cf31e082
XFNP <cfarrow29@jacksonindians.us>
2026-09-15 21:09:00 -0500

    Update screen.py

--- cwio/screen.py
+++ cwio/screen.py
@@ -223,7 +223,6 @@ def write_char(c: str, x: int, y: int, v: int, font: Font) -> None:
 
 
 def write(s: str, x: int, y: int, v: int, font: Font) -> None:
-    print("WRITE:", hex(addr), hex(data))
     f = font()
     ox = 0
     oy = 0
