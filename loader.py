87e57982fce8031a848d0a8b6e1e8ee0c925c18e
XFNP <cfarrow29@jacksonindians.us>
2026-09-15 20:12:13 -0500

    Update loader.py

--- loader.py
+++ loader.py
@@ -23,7 +23,7 @@ class EntryPointError(Error):
 
 
 apps = []
-for path in os.listdir("/apps"):
+for path in os.listdir("apps"):
     if path.endswith(".py") and (not path.startswith("_")) and fs.isfile("/apps/" + path):
         name = path[:-3]
         module = getattr(__import__("apps." + name), name)
