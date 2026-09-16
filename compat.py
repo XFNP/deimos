f7c687b263d19b15dd189525e12c4aa728e893e4
XFNP <cfarrow29@jacksonindians.us>
2026-09-15 19:58:47 -0500

    Create compat.py

---
+++ compat.py
@@ -0,0 +1,19 @@
+import time
+
+def sleep_ms(ms):
+    time.sleep(ms / 1000.0)
+
+def sleep_us(us):
+    time.sleep(us / 1_000_000.0)
+
+def ticks_ms():
+    return time.monotonic_ns() // 1_000_000
+
+def ticks_us():
+    return time.monotonic_ns() // 1_000
+
+def ticks_cpu():
+    return time.monotonic_ns()
+
+def ticks_diff(a, b):
+    return a - b
