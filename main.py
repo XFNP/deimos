1f6a572715be53b0eb69168e3ea3740f796381d3
XFNP <cfarrow29@jacksonindians.us>
2026-09-15 21:19:11 -0500

    Update main.py

--- main.py
+++ main.py
@@ -54,81 +54,81 @@ def run_app(app):
     protected_run(run)
 
 
-try:
-    while True:
-        print("LOOP START")
-    
-        print("CLEAR")
-        screen.clear()
-    
-        print("RENDER")
-        scr.render()
-    
-        print("APPLY")
-        screen.apply()
-    
-        print("AFTER APPLY")
-    
-        key = keyboard.get_next()
-        key = keyboard.get_next()
-        if key == KB.KEY.SETTINGS:
-            c = choose(["Updates", "WiFi", "Power", "Debug"])
-            if c == 0:
-                if wifi.support:
-                    c = choose(("From OFW repo", "From custom repo"))
-                    if c >= 0:
-                        if wifi.choose_connect():
-                            if c == 0:
+
+while True:
+    print("LOOP START")
+
+    print("CLEAR")
+    screen.clear()
+
+    print("RENDER")
+    scr.render()
+
+    print("APPLY")
+    screen.apply()
+
+    print("AFTER APPLY")
+
+    key = keyboard.get_next()
+    key = keyboard.get_next()
+    if key == KB.KEY.SETTINGS:
+        c = choose(["Updates", "WiFi", "Power", "Debug"])
+        if c == 0:
+            if wifi.support:
+                c = choose(("From OFW repo", "From custom repo"))
+                if c >= 0:
+                    if wifi.choose_connect():
+                        if c == 0:
+                            ui.notify("Updating", "OS will restart automatically after the update.", res.ui.info, wait = False)
+                            garbage.collect()
+                            if protected_run(update.download):
+                                print("[FWUP] Rebooting device")
+                                machine.reset()
+                            else:
+                                ui.notify("Update error", "Something went wrong while downloading the\nupdate.", res.ui.error)
+                        elif c == 1:
+                            repo = ui.ask("GitHub repo to fetch from:", update.OFW.REPO)
+                            if repo.count("/") == 1:
                                 ui.notify("Updating", "OS will restart automatically after the update.", res.ui.info, wait = False)
+                                
                                 garbage.collect()
-                                if protected_run(update.download):
+                                # lambda macro
+                                if protected_run((lambda: update.download(repo))):
                                     print("[FWUP] Rebooting device")
                                     machine.reset()
                                 else:
-                                    ui.notify("Update error", "Something went wrong while downloading the\nupdate.", res.ui.error)
-                            elif c == 1:
-                                repo = ui.ask("GitHub repo to fetch from:", update.OFW.REPO)
-                                if repo.count("/") == 1:
-                                    ui.notify("Updating", "OS will restart automatically after the update.", res.ui.info, wait = False)
-                                    
-                                    garbage.collect()
-                                    # lambda macro
-                                    if protected_run((lambda: update.download(repo))):
-                                        print("[FWUP] Rebooting device")
-                                        machine.reset()
-                                    else:
-                                        ui.notify("Update error", "Something went wrong while downloading the\nupdate. Are you sure the repository you\nspecified exists?", res.ui.error)
-                                else:
-                                    ui.notify("Invalid repo", "https://github.com/" + repo + "\nis not a valid GitHub repo.", res.ui.error)
-                            
-                        else:
-                            ui.notify("No connection", "A stable Internet connection is required to\nperform updates.", res.ui.error)
-                else:
-                    ui.notify("No WiFi support", "WiFi support is required to perform updates.", res.ui.error)
+                                    ui.notify("Update error", "Something went wrong while downloading the\nupdate. Are you sure the repository you\nspecified exists?", res.ui.error)
+                            else:
+                                ui.notify("Invalid repo", "https://github.com/" + repo + "\nis not a valid GitHub repo.", res.ui.error)
+                        
+                    else:
+                        ui.notify("No connection", "A stable Internet connection is required to\nperform updates.", res.ui.error)
+            else:
+                ui.notify("No WiFi support", "WiFi support is required to perform updates.", res.ui.error)
+        elif c == 1:
+            if wifi.support:
+                wifi.choose_connect()
+            else:
+                ui.notify("No WiFi support", "WiFi support is required to connect to a\nnetwork.", res.ui.error)
+        elif c == 2:
+            c = choose(["Deep sleep", "Soft reset", "Hard reset"])
+            if c == 0:
+                power.deepsleep()
             elif c == 1:
-                if wifi.support:
-                    wifi.choose_connect()
-                else:
-                    ui.notify("No WiFi support", "WiFi support is required to connect to a\nnetwork.", res.ui.error)
+                machine.soft_reset()
             elif c == 2:
-                c = choose(["Deep sleep", "Soft reset", "Hard reset"])
-                if c == 0:
-                    power.deepsleep()
-                elif c == 1:
-                    machine.soft_reset()
-                elif c == 2:
-                    machine.reset()
-                
-            elif c == 3:
-                ui.notify("Debug mode", "Control has been redirected to USB", res.ui.info, wait = False)
-                raise DebugInterrupt("Returning to REPL")
+                machine.reset()
             
-        elif key == KB.KEY.HOME:
-            c = choose(applist)
-            if c >= 0:
-                run_app(apps[c][0])
-        elif key == KB.KEY.AC:
-            power.deepsleep()
+        elif c == 3:
+            ui.notify("Debug mode", "Control has been redirected to USB", res.ui.info, wait = False)
+            raise DebugInterrupt("Returning to REPL")
+        
+    elif key == KB.KEY.HOME:
+        c = choose(applist)
+        if c >= 0:
+            run_app(apps[c][0])
+    elif key == KB.KEY.AC:
+        power.deepsleep()
         
 except DebugInterrupt as e:
     print("DEBUG: " + (str(e)))
